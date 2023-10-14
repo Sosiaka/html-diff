9444409b44c3de8eb1d5059bd4fb089ad8b8da2b
Alexander Borisov <113920954+Sosiaka@users.noreply.github.com>
2023-10-14 18:22:01 +0300

    Update htmldiff.go

--- htmldiff.go
+++ htmldiff.go
@@ -39,7 +39,7 @@ type Config struct {
 // HTMLdiff finds all the differences in the versions of HTML snippits,
 // versions[0] is the original, all other versions are the edits to be compared.
 // The resulting merged HTML snippits are as many as there are edits to compare.
-func (c *Config) HTMLdiff(versions []string, T time.Duration) ([]string, error) {
+func (c *Config) HTMLdiff(versions []string) ([]string, error) {
 	if len(versions) < 2 {
 		return nil, errors.New("there must be at least two versions to diff, the 0th element is the base")
 	}
@@ -92,7 +92,7 @@ func (c *Config) HTMLdiff(versions []string, T time.Duration) ([]string, error) 
 			go func(ch chan []diff.Change) {
 				ch <- diff.Diff(len(*sourceTreeRunes[0]), len(*sourceTreeRunes[m+1]), dd)
 			}(ch)
-			to := time.After(time.Second * T)
+			to := time.After(time.Second * 35)
 			select {
 			case <-to:
 				parallelErrors <- errors.New("diff.Diff() took too long")
